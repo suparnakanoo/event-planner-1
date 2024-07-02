601f0ba5c07214ee5bec5d3aa64f699722fb5060
Suparna Kanoo <kanoosuparna@gmail.com>
2024-07-02 20:48:51 +0530

    first commit

---
+++ event-planner-1/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import EventPlanner from "./Components/EventPlanner";
+
+function App() {
+  return (
+    <>
+      <EventPlanner />
+    </>
+  );
+}
+export default App;
---
+++ event-planner-1/src/Components/EventPlanner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./EventPlanner.css";
+
+const EventPlanner = () => {
+  return (
+    <div className="event-planner-container">
+      <header>
+        <h1>Welcome to Event Planner</h1>
+      </header>
+    </div>
+  );
+};
+
+export default EventPlanner;
